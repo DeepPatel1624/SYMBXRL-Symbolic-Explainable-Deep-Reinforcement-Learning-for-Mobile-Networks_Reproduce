0de0f8d3b6b8585076bc915177e52a2d3957b185
Deep Patel <deep.p4@ahduni.edu.in>
2026-03-04 16:39:27 +0530

    Added the overll function definations skeleton

--- custom_mimo_env.py
+++ custom_mimo_env.py
@@ -197,5 +197,142 @@ class MimoEnv(gym.Env):
 
         return reward
     
+    def set_state(self, state):
+        pass
+
+        """
+            Set the environment to a specific state.
+            Args:
+                state (numpy.ndarray): State to set the environment to.
+        """
+
+    def getinfo(self):
+        """
+            Get information about the environment.
+            Returns:
+                dict: Information about the environment.
+        """
+        pass
+
+
+    def update_history():
+        """
+            Update the history of the environment.
+            Args:
+                info (dict): Information about the environment.
+        """
+        pass
+
+
+    def calculate_reward():
+        pass
+
     
-        
+    def __call(self):
+        return self
+
+
+# Extra functions
+
+def transform_input_to_output():
+    """
+        Transform input action to binary coded output action.
+        Args:
+            input_sequence (list): Input sequence [1,3,4].
+            total_variables (int): Total number of users 7.
+        Returns:
+            list: Output sequence  [0 1 0 1 1 0 0].
+    """
+    pass
+
+def transform_array():
+    """
+        Transform the array based on maximum occurrences.
+        Args:
+            arr (list): Input array.
+        Returns:
+            list: Transformed array.
+    """
+    pass
+
+def get_selected_indices_and_values():
+    """
+        Get selected indices and their values from the array.
+        Args:
+            arr (list): Input array.
+        Returns:
+            tuple: Number of selected indices and their values.
+    """
+
+    pass
+
+def count_occurrences():
+    """
+        This function counts the maximum occurrence of a variable in an array.
+        Args:
+            arr: A list of integers.
+        Returns:
+            A tuple containing the variable with the maximum occurrence and its count.
+    """
+    pass
+
+
+def adjust_se_interfernce():
+    """
+    Adjust the spectral efficiency based on the interference.
+    Args:
+        non_zero_elements (list): Non-zero elements.
+        ur_se (numpy.ndarray): Spectral efficiency for each user.
+        ur_se_total (float): Total spectral efficiency.
+        usrgrp (int): User group index.
+        ue_select (int): Selected user index.
+    Returns:
+        numpy.ndarray: Adjusted spectral efficiency.
+        float: Adjusted total spectral efficiency.
+    """
+
+    pass
+
+def usr_group():
+    """
+        This function groups users based on the correlation of their channel vectors(CSI).
+        Parameters:
+        H (numpy.ndarray): A matrix of channel vectors where each column corresponds to a user and each row corresponds to a base station antenna.
+        Returns:
+        numpy.ndarray: An array where each element represents the group index of the corresponding user.
+    """    
+    pass
+
+def data_process():
+    """
+        This function converts channel vectors to Spectral efficiency per user and SINR per user.
+        Parameters:
+            H (numpy.ndarray): A matrix of channel vectors where each column corresponds to a user and each row corresponds to a base station antenna.
+            N_UE: No of Users
+            MOD_ORDER: Modulation Order
+        Returns:
+            System Spectral Efficiency, SINR (all users), spectral effecincy (all users)
+    """    
+    pass
+
+def modulation():
+    '''
+        xzSub Functions of Previous Main Function - Data Process
+    '''
+    pass
+
+def demodulation (mod_order, data):
+    pass
+
+def sel_ue():
+    '''
+         Converting Action into User indexed action
+    '''
+    pass
+
+def reverse_sel_ue():
+    '''
+         Reversing User Indexed action to system action
+    '''
+    pass
+
